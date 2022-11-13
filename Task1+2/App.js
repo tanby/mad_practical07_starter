aa13107a319023c042556e869e2afe013717337d
tanby <tan_boon_yuen@ichat.sp.edu.sg>
2022-11-13 17:42:28 +0800

    added p7 starter

---
+++ Task1+2/App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+//-- add code for practical
+  //-- Task 1: Create a SectionList 
+  //-- Task 2: Modify the SectionList java script coding to enable the display an Alert message. 
+  // The alert message will display the name selected. 
+  
+const App = ()=>{
+  
+
+}
+
+export default App
